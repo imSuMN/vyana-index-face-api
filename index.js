1d00e89f3892405d3fae2293a3716059da85a56d
SUMAN KUMAR <86798449+imSuMN@users.noreply.github.com>
2023-08-29 23:48:40 +0530

    Nothing much

--- index.js
+++ index.js
@@ -18,18 +18,18 @@ app.use(
   express.urlencoded({ parameterLimit: 100000, limit: "50mb", extended: true })
 );
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "x-access-token, Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "x-access-token, Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.use(cors());
 
